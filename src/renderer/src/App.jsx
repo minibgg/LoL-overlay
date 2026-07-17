b1e96ce4c8dbc7b8278edda023ea897ff80a653e
minibgg <ivangey984@gmail.com>
2026-07-17 14:02:32 +0300

    fetch data

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,3 +1,16 @@
+import { useState } from 'react'
+
 export default function App() {
+  const [gameInfo, setGameInfo] = useState(null)
+  try {
+    async function fetchGameInfo() {
+      const res = await fetch('https://127.0.0.1:2999/liveclientdata/allgamedata')
+      const data = await res.json
+      setGameInfo(data)
+    }
+  } catch (error) {
+    console.error('fetch error', error)
+  }
+
   return <div>123123123123</div>
 }
