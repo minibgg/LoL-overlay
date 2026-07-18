76659810b05c011618efd2462ec09894a1f738c2
minibgg <ivangey984@gmail.com>
2026-07-18 12:25:39 +0300

    change csm gpm metod + add kda

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -5,16 +5,25 @@ import { riotApi } from './components/Api'
 export default function App() {
   const [gameInfo, setGameInfo] = useState(null)
   const [activePlayerInfo, setActivePlayerInfo] = useState(null)
+  const [goldPerMinute, setGoldPerMinute] = useState('')
+  const [creepScorePerMinute, setCreepScorePerMinute] = useState('')
+  const [killDeathAssist, setKillDeathAssist] = useState('')
+
+  function getScoreClass(value) {
+    const num = Number(value)
+    if (num >= 9) return 'good-score'
+    if (num >= 7) return 'normal-score'
+    return 'bad-score'
+  }
 
   useEffect(() => {
     async function fetchGameInfo() {
       try {
+        const gameEvents = await riotApi.gameEvents()
         const playersInfo = await riotApi.playersInfo()
         setActivePlayerInfo(
           playersInfo.allPlayers.filter((p) => p.riotId === playersInfo.activePlayer.riotId)
         )
-        const gameEvents = await riotApi.gameEvents()
-
         setGameInfo(playersInfo)
       } catch (error) {
         console.error('fetch error', error)
@@ -26,27 +35,35 @@ export default function App() {
     return () => clearInterval(intervalId)
   }, [])
 
+  useEffect(() => {
+    if (activePlayerInfo === null || gameInfo === null) return
+    setGoldPerMinute(
+      (gameInfo?.activePlayer?.currentGold / (gameInfo?.gameData?.gameTime / 60)).toFixed(2)
+    )
+    setCreepScorePerMinute(
+      (activePlayerInfo[0]?.scores?.creepScore / (gameInfo?.gameData?.gameTime / 60)).toFixed(2)
+    )
+    const kda =
+      (activePlayerInfo[0]?.scores?.kills + activePlayerInfo[0]?.scores?.assists) /
+      activePlayerInfo[0]?.scores?.deaths
+
+    setKillDeathAssist(Number.isNaN(kda) ? '0' : kda.toFixed(2))
+  }, [activePlayerInfo, gameInfo])
+
   if (gameInfo == null) {
     return (
       <div>
-        <div className="drag-region" style={{ height: 30, width: '100%' }}></div>
+        <div Id="drag-region" style={{ height: 30, width: '100%' }}></div>
         <div className="main-info">ожидание игры</div>
       </div>
     )
   } else {
     return (
       <div>
-        <div className="drag-region" style={{ height: 30, width: '100%' }}></div>
-        <div className="main-info">
-          CS:
-          {(activePlayerInfo[0]?.scores?.creepScore / (gameInfo?.gameData?.gameTime / 60)).toFixed(
-            2
-          )}
-        </div>
-        <div>
-          GPM:{' '}
-          {(gameInfo?.activePlayer?.currentGold / (gameInfo?.gameData?.gameTime / 60)).toFixed(2)}
-        </div>
+        <div id="drag-region" style={{ height: 30, width: '100%' }}></div>
+        <div className={getScoreClass(creepScorePerMinute)}>CS: {creepScorePerMinute}</div>
+        <div className={getScoreClass(goldPerMinute)}>GPM: {goldPerMinute}</div>
+        <div>KDA: {killDeathAssist}</div>
       </div>
     )
   }
